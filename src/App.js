c9305c0a5d071bddff16555dcb0898e02a42f16b
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-28 12:02:05 +0300

    created template and initial files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Col, Container, Row } from "react-bootstrap";
+import Table from "./components/Table";
+import Detail from "./components/Detail";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <Row>
+            <Col>
+              <Table />
+            </Col>
+            <Col className="my-auto">
+              <Detail />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import { ListGroup } from "react-bootstrap";
+
+class Table extends Component {
+  state = {
+    selected: undefined,
+  };
+
+  checkSelected = (value) => (value === this.state.selected ? "selected" : "");
+
+  render() {
+    return (
+      <>
+        <ListGroup>
+          <ListGroup.Item
+            onClick={(e) => this.setState({ selected: "First" })}
+            className={this.checkSelected("First")}
+          >
+            First
+          </ListGroup.Item>
+          <ListGroup.Item
+            onClick={(e) => this.setState({ selected: "Second" })}
+            className={this.checkSelected("Second")}
+          >
+            Second
+          </ListGroup.Item>
+          <ListGroup.Item
+            onClick={(e) => this.setState({ selected: "Third" })}
+            className={this.checkSelected("Third")}
+          >
+            Third
+          </ListGroup.Item>
+        </ListGroup>
+        <p className="mt-3">
+          Local state is {this.state.selected || "undefined"}
+        </p>
+      </>
+    );
+  }
+}
+
+export default Table;
